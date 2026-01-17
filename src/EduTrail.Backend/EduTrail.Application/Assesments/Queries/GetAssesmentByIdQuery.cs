c2916d77c9e49c64fb91afaf1e42c7cb483c1cac
prianka-0011 <priankanew0011@gmail.com>
2026-01-16 23:33:45 -0600

    .

--- src/EduTrail.Backend/EduTrail.Application/Assesments/Queries/GetAssesmentByIdQuery.cs
+++ src/EduTrail.Backend/EduTrail.Application/Assesments/Queries/GetAssesmentByIdQuery.cs
@@ -1,12 +1,24 @@
+using AutoMapper;
+using EduTrail.Application.CourseOfferings;
 using MediatR;
 
 namespace EduTrail.Application.Assesments
 {
     public class GetAssesmentByIdQuery : IRequest<AssesmentDto>
     {
-        public class GetAssesmentByIdQuery : IRequestHandler<GetAssesmentByIdQuery, AssesmentDto>
+        public class Handler : IRequestHandler<GetAssesmentByIdQuery, AssesmentDto>
         {
-            
+            private readonly IAssesmentRepository _repository;
+            private readonly IMapper _mapper;
+            public Handler(IAssesmentRepository repository, IMapper mapper)
+            {
+                _repository = repository;
+            }
+
+            public Task<AssesmentDto> Handle(GetAssesmentByIdQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
