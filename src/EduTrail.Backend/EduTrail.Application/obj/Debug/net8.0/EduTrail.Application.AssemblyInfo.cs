44998e5169d44c3ce8e71df096ad5754574f07df
prianka-0011 <priankanew0011@gmail.com>
2026-01-17 16:18:52 -0600

    .

--- src/EduTrail.Backend/EduTrail.Application/obj/Debug/net8.0/EduTrail.Application.AssemblyInfo.cs
+++ src/EduTrail.Backend/EduTrail.Application/obj/Debug/net8.0/EduTrail.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduTrail.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f598ef215a74fb91ccea76f21b4f32325d99ab98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a15557f6a29dcd0498c7b31893b67c25de88dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduTrail.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduTrail.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
