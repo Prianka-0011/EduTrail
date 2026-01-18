7844477a4be0eb7cc43804ecb0ac608b7dea8f66
prianka-0011 <priankanew0011@gmail.com>
2026-01-17 22:23:18 -0600

    .

--- src/EduTrail.Backend/EduTrail.Application/Assesments/Commands/UpdateAssesmentCommant.cs
+++ src/EduTrail.Backend/EduTrail.Application/Assesments/Commands/UpdateAssesmentCommant.cs
@@ -1,4 +1,24 @@
+using AutoMapper;
+using MediatR;
+
 namespace EduTrail.Application.Assesments
 {
-    
+    public class UpdateAssesmentCommant : IRequest<AssesmentDto>
+    {
+        public class Handler : IRequestHandler<UpdateAssesmentCommant, AssesmentDto>
+        {
+            private readonly IAssesmentRepository _repository;
+            private readonly IMapper _mapper;
+            public Handler(IAssesmentRepository repository, IMapper mapper)
+            {
+                _repository = repository;
+                _mapper = mapper;
+            }
+
+            public Task<AssesmentDto> Handle(UpdateAssesmentCommant request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
 }
