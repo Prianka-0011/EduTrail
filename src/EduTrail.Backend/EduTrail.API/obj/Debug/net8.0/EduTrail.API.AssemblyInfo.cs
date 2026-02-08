89d3914e72a33b1cd347ae4d89739801d2f1022f
prianka-0011 <priankanew0011@gmail.com>
2026-02-07 21:21:01 -0600

    .

--- src/EduTrail.Backend/EduTrail.API/obj/Debug/net8.0/EduTrail.API.AssemblyInfo.cs
+++ src/EduTrail.Backend/EduTrail.API/obj/Debug/net8.0/EduTrail.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduTrail.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3595abd7a046ceb4096e9c3bf0a64b61a21e2406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deeea7afda60095da0ff61e8cdf52a025ac43c75")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduTrail.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduTrail.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/EduTrail.Backend/EduTrail.Application/obj/Debug/net8.0/EduTrail.Application.AssemblyInfo.cs
+++ src/EduTrail.Backend/EduTrail.Application/obj/Debug/net8.0/EduTrail.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduTrail.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3595abd7a046ceb4096e9c3bf0a64b61a21e2406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deeea7afda60095da0ff61e8cdf52a025ac43c75")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduTrail.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduTrail.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
