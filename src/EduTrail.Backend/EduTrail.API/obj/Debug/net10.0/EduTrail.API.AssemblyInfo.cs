bc7e38ec4d66f7b930b87042c4612b5377ce586e
prianka-0011 <priankanew0011@gmail.com>
2026-02-27 15:02:13 -0600

    Remove DLL files from tracking

--- src/EduTrail.Backend/EduTrail.API/obj/Debug/net10.0/EduTrail.API.AssemblyInfo.cs
+++ src/EduTrail.Backend/EduTrail.API/obj/Debug/net10.0/EduTrail.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduTrail.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad1f5d3ffd830780594a7d9bf3589011d5b529d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c4fc901d76be5dc515828b883708038ea21c6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduTrail.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduTrail.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
