a5a3b22cb527714c4e85fdf915a513d660aaafbe
prianka-0011 <priankanew0011@gmail.com>
2026-03-24 09:14:24 -0500

    working on bulk amount enrolement

--- src/EduTrail.Backend/EduTrail.Application/obj/Debug/net10.0/EduTrail.Application.AssemblyInfo.cs
+++ src/EduTrail.Backend/EduTrail.Application/obj/Debug/net10.0/EduTrail.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduTrail.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c429b75328cdda2d7e63c8bfd4fee88a0b1aa757")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08a2c774104b60d71b2be9e9bd8d712458dfeb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduTrail.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduTrail.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
