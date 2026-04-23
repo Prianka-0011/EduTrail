940d6a4fc9d65045192ca3a614b889f6e9dc12ff
prianka-0011 <priankanew0011@gmail.com>
2026-04-22 23:16:05 -0500

    correct file name case

--- src/EduTrail.Backend/EduTrail.API/obj/Debug/net10.0/EduTrail.API.AssemblyInfo.cs
+++ src/EduTrail.Backend/EduTrail.API/obj/Debug/net10.0/EduTrail.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduTrail.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145d165d596c5b2dc31983d757beec6270a8f4d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c8389173281e37da837e48bf5cae222421a07cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduTrail.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduTrail.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
